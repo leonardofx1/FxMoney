d31540a4cd84f9467af8aec59c6643abb7129866
leonardo <102767490+leonardofx1@users.noreply.github.com>
2023-12-22 15:18:09 -0300

    refactor

--- fxmoney/src/context/TransactionProvider.tsx
+++ fxmoney/src/context/TransactionProvider.tsx
@@ -1,51 +1,35 @@
-import { createContext,  useState } from "react";
-import {  TransactionType } from '../@types/types';
-
+import { createContext, useState, Dispatch, SetStateAction } from "react";
+import { TransactionType } from '../@types/types';
 
 export type TransactionContextType = {
     transaction: TransactionType[] 
     setTransactionStorage: (newTransaction:  TransactionType) => void
     getTransactionStorage: (arg:string) => void
-    setTransaction: (state:TransactionType[] | TransactionType) => void
+    setTransaction: Dispatch<SetStateAction<TransactionType[]>>
 }
 
 export const transactionContext = createContext<TransactionContextType | null>(null)
 
 export const TransactionProvider = ({ children }: { children: React.ReactNode }) => {
-    const [transaction, setTransaction] = useState< TransactionType[] | []>([])
+    const [transaction, setTransaction] = useState<TransactionType[]>([]);
 
     const getTransactionStorage = (key:string) => {
-       
-        const getTransaction: TransactionType[] = JSON.parse(localStorage.getItem(key)!)
-        getTransaction !== null && setTransaction(getTransaction)
-      
+        const getTransaction: TransactionType[] | null = JSON.parse(localStorage.getItem(key)!) || null;
+        getTransaction && setTransaction(getTransaction);
     }
+
     const setTransactionStorage = (newTransaction:TransactionType) => {
+        const historyTransaction = JSON.parse(localStorage.getItem('transaction')!) || [];
 
+        const existingTransactions = [...historyTransaction, newTransaction];
+        setTransaction(existingTransactions);
 
-        const historyTransaction = localStorage.getItem('transaction')
-
-        if (historyTransaction !== null) {
-            const existingTransactions = JSON.parse(historyTransaction);
-            existingTransactions[existingTransactions.length] = newTransaction
-
-            setTransaction(existingTransactions)
-
-
-            const updatedTransactions = JSON.stringify(existingTransactions);
-            localStorage.setItem('transaction', updatedTransactions);
-        } else {
-            localStorage.setItem('transaction', JSON.stringify([newTransaction]));
-            getTransactionStorage('transaction')
-        }
-
-
-
+        const updatedTransactions = JSON.stringify(existingTransactions);
+        localStorage.setItem('transaction', updatedTransactions);
     }
-
 
     return (
-        <transactionContext.Provider value={{ setTransactionStorage, transaction, getTransactionStorage, setTransaction}}>
+        <transactionContext.Provider value={{ setTransactionStorage, transaction, getTransactionStorage, setTransaction }}>
             {children}
         </transactionContext.Provider>
     )
--- fxmoney/src/utils/formatDatePrice.ts
+++ fxmoney/src/utils/formatDatePrice.ts
@@ -3,8 +3,7 @@
 export const formatCurrency = (price: number) => new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
   price
 )
- 
-type dateType =  string 
+
 export const formatDate = (date:Date) => {
 
 
